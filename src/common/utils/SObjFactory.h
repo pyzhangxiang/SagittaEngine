97eaf4834fcea8246afc9158abe4d05436e5b43f
zhangxiang <jieyar@gmail.com>
2009-10-04 18:59:24 +0000

    add SObjFactory.h
    added SObjFactory.h

---
+++ src/common/utils/SObjFactory.h
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////
+// file: SObjFactory.h 
+// created by zhangxiang on 09-10-4
+// declares of the class SObjFactory
+// SObjFactory is a class ...
+//////////////////////////////////////////////////////
+#ifndef __SOBJFACTORY_H__
+#define __SOBJFACTORY_H__
+
+// INCLUDES //////////////////////////////////////////
+
+// DECLARES //////////////////////////////////////////
+
+namespace Sagitta{
+	
+	/** class representation
+	 @remarks
+	 
+	 @note
+	 
+	 @par
+	 
+	 */
+	class _SagittaExport SObjFactory{
+	// member variables
+		
+	// constructors & destructor
+		
+	// member functions
+		
+	}; //#### end class SObjFactory
+	
+} // namespace Sagitta
+
+// DEFINES ///////////////////////////////////////////
+
+#endif // __SOBJFACTORY_H__
+
