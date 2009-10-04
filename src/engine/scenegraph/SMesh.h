2c49a28f6d18891cc6bcfd862efc2d6d9a76f1dd
zhangxiang <jieyar@gmail.com>
2009-10-04 19:31:43 +0000

    added SMesh
    added SMesh

---
+++ src/engine/scenegraph/SMesh.h
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////
+// file: SMesh.h 
+// created by zhangxiang on 09-10-4
+// declares of the class SMesh
+// SMesh is a class ...
+//////////////////////////////////////////////////////
+#ifndef __SMESH_H__
+#define __SMESH_H__
+
+// INCLUDES //////////////////////////////////////////
+
+// DECLARES //////////////////////////////////////////
+
+namespace Sagitta{
+	
+	/** class representation
+	 @remarks
+	 
+	 @note
+	 
+	 @par
+	 
+	 */
+	class _SagittaExport SMesh{
+	// member variables
+		
+	// constructors & destructor
+		
+	// member functions
+		
+	}; //#### end class SMesh
+	
+} // namespace Sagitta
+
+// DEFINES ///////////////////////////////////////////
+
+#endif // __SMESH_H__
+
