5809c02be22e0d47387e1b9f02511f0f2f90765e
zhangxiang <jieyar@gmail.com>
2009-10-04 19:39:15 +0000

    added SMeshDelegate
    added SMeshDelegate

---
+++ src/engine/resource/SMeshDelegate.h
@@ -0,0 +1,38 @@
+//////////////////////////////////////////////////////
+// file: SMeshDelegate.h 
+// created by zhangxiang on 09-10-4
+// declares of the class SMeshDelegate
+// SMeshDelegate is a class ...
+//////////////////////////////////////////////////////
+#ifndef __SMESHDELEGATE_H__
+#define __SMESHDELEGATE_H__
+
+// INCLUDES //////////////////////////////////////////
+
+// DECLARES //////////////////////////////////////////
+
+namespace Sagitta{
+	
+	/** class representation
+	 @remarks
+	 
+	 @note
+	 
+	 @par
+	 
+	 */
+	class _SagittaExport SMeshDelegate{
+	// member variables
+		
+	// constructors & destructor
+		
+	// member functions
+		
+	}; //#### end class SMeshDelegate
+	
+} // namespace Sagitta
+
+// DEFINES ///////////////////////////////////////////
+
+#endif // __SMESHDELEGATE_H__
+
